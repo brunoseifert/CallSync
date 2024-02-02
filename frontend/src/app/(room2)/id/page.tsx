8cfbc38f5e12adc766ec0882af84666a968b2b38
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-02-02 15:31:42 -0300

    feat: Chat component with video page

--- frontend/src/app/(room2)/id/page.tsx
+++ frontend/src/app/(room2)/id/page.tsx
@@ -1,11 +1,34 @@
+import Chat from "@/components/Chat";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-export default function Id() {
+export default function Room({ params }: { params: { id: string } }) {
   return (
-    <div>
+    <div className="max-h-screen">
       <Header />
-      <h1>id</h1>
+      <div className="flex h-[80%] ">
+        <div className="md:w-[80%] max-w-7xl w-full mx-auto m-3 ">
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
+            <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
+              <video className="h-full w-full"></video>
+              <span className="absolute bottom-3">Alexia Kattah</span>
+            </div>
+            <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
+              <video className="h-full w-full"></video>
+              <span className="absolute bottom-3">Alexia Kattah</span>
+            </div>
+            <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
+              <video className="h-full w-full"></video>
+              <span className="absolute bottom-3">Alexia Kattah</span>
+            </div>
+            <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
+              <video className="h-full w-full"></video>
+              <span className="absolute bottom-3">Alexia Kattah</span>
+            </div>
+          </div>
+        </div>
+        <Chat />
+      </div>
       <Footer />
     </div>
   );
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -18,9 +18,9 @@ export default function Footer() {
   const hours = date.getHours().toString().padStart(2, "0") + ":";
   const minutes = date.getMinutes().toString().padStart(2, "0");
   return (
-    <div className="fixed items-center bottom-0 bg-black py-6 w-full">
+    <div className="fixed items-center bottom-0  bg-black py-6 w-full">
       <div className="grid grid-cols-3 ">
-        <div className="flex items-center">
+        <div className="flex items-center mx-auto">
           <p className="text-xl">
             {hours}
             {minutes}
