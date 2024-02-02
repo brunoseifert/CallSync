319c4cdfe170ca0aa92e1ffee19528a668e74d4a
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-02-02 10:47:20 -0300

    feat: compenents svgr create

---
+++ frontend/src/Icons/index.ts
@@ -0,0 +1,8 @@
+export { default as Camera } from "./Camera";
+export { default as Computer } from "./Computer";
+export { default as Logo } from "./Logo";
+export { default as Mic } from "./Mic";
+export { default as NoCamera } from "./NoCamera";
+export { default as NoComputer } from "./NoComputer";
+export { default as NoMic } from "./NoMic";
+export { default as Phone } from "./Phone";
